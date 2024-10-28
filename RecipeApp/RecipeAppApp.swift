27eaaf868a7930638c2cbf7abc9c35b1c4e219f2
Shawn Cole <Cole9350@gmail.com>
2024-10-28 11:58:30 -0400

    Initial project creation.

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Shawn Cole on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
